5b19ec6f073f04283ccae7b1f71e74d53ad9cdef
Rakha Fatih <rakhafatihathallah@gmail.com>
2024-02-29 09:11:47 +0700

    Initial Commit

---
+++ Assignment-3-MotionLab/Assignment_3_MotionLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_3_MotionLabApp.swift
+//  Assignment-3-MotionLab
+//
+//  Created by Rakha Fatih Athallah on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_3_MotionLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
